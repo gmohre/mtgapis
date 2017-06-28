f147222b4fe636278b308087ce232b94e9219094
glenn mohre <glenn.mohre@gmail.com>
2017-06-28 11:34:29 -0400

    moved secret key to local file

--- mtgapis/mtgapis/settings.py
+++ mtgapis/mtgapis/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+from local_settings import SECRET_KEY
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'kt(ch-x-k0ofu=ci6&xw1l&(!kz%5tt9rl77ri442j)pi$f+*q'
+SECRET_KEY = SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
